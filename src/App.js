f546ad4ee48441725a15a0ccdbc57b1753f40e19
Yujeonja <hardboiled65@gmail.com>
2021-06-21 15:07:24 +0900

    Final page done without design.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Step2 from './Step2'
 import Step3 from './Step3'
 import Step4 from './Step4'
 import Step5 from './Step5'
+import StepFinal from './StepFinal'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Route path='/step3' component={Step3} />
       <Route path='/step4' component={Step4} />
       <Route path='/step5' component={Step5} />
+      <Route path='/final' component={StepFinal} />
     </div>
   );
 }
---
+++ src/StepFinal.js
@@ -0,0 +1,118 @@
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+// import './StepFinal.scss'
+
+import BaseButton from './components/BaseButton'
+import store from './store/index'
+
+const StepFinal = () => {
+  const [serviceType, setServiceType] = useState(null);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const featurePrices = {
+    additionalFeatures: {
+      gps: 200,
+      map: 200,
+      kakaoPush: 200,
+      community: 200,
+      share: 100,
+      userTypeExpand: 200,
+      chat: 200,
+      none: 0,
+    },
+    admin: {
+      inService: 100,
+      defaultAdmin: 150,
+      extendedAdmin: 200,
+      none: 0,
+    },
+  };
+
+  function calculatePrice() {
+    let basePrice = 0;
+    let featurePrice = 0;
+
+    basePrice += store.getState().serviceTypePrice;
+    basePrice += store.getState().detailPlanPrice;
+    basePrice += store.getState().designPrice;
+    basePrice = basePrice * store.getState().pageMultiple;
+
+    const additionalFeatures = store.getState().additionalFeatures;
+    featurePrice += additionalFeatures.gps
+      ? featurePrices.additionalFeatures.gps
+      : 0;
+    featurePrice += additionalFeatures.map
+      ? featurePrices.additionalFeatures.map
+      : 0;
+    featurePrice += additionalFeatures.kakaoPush
+      ? featurePrices.additionalFeatures.kakaoPush
+      : 0;
+    featurePrice += additionalFeatures.community
+      ? featurePrices.additionalFeatures.community
+      : 0;
+    featurePrice += additionalFeatures.share
+      ? featurePrices.additionalFeatures.share
+      : 0;
+    featurePrice += additionalFeatures.userTypeExpand
+      ? featurePrices.additionalFeatures.userTypeExpand
+      : 0;
+    featurePrice += additionalFeatures.chat
+      ? featurePrices.additionalFeatures.chat
+      : 0;
+
+    const admin = store.getState().admin;
+    if (admin.inService === true) {
+      featurePrice += featurePrices.admin.inService;
+    } else if (admin.defaultAdmin === true) {
+      featurePrice += featurePrices.admin.defaultAdmin;
+    } else if (admin.extendedAdmin === true) {
+      featurePrice += featurePrices.admin.extendedAdmin;
+    }
+
+    const total = basePrice + featurePrice;
+
+    return total;
+  }
+
+  useEffect(() => {
+    const serviceTypePrice = store.getState().serviceTypePrice;
+    if (serviceTypePrice === 300) {
+      setServiceType('App');
+    } else if (serviceTypePrice === 400) {
+      setServiceType('Web');
+    }
+  }, []);
+
+  useEffect(() => {
+    setTotalPrice(calculatePrice());
+  }, []);
+
+  const history = useHistory();
+
+  function goFirst() {
+    history.push('/');
+  }
+
+  return (
+    <div className="StepFinal">
+      <div>수고하셨습니다.</div>
+      <div>선택 프로젝트 : { serviceType }</div>
+      <div>견적 가격 : 대략 { totalPrice }만원</div>
+      <div>입니다.</div>
+      <div className="detail">하지만 정확한 견적은 아니에요 :)<br />
+티릴리와 함께 같이 진단해보고 멋진 서비스를 만들어보아요.</div>
+      <BaseButton
+      glow={true}
+      onClick={goFirst}>다시하기
+      </BaseButton>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return { admin: state.admin };
+};
+
+export default connect()(StepFinal)
